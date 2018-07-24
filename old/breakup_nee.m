1388420f688ca8eac2bb9af0194026c45a4429e1
Grey Nearing <gsnearing@BE-1036-GSNearing.local>
2018-07-23 19:44:35 -0500

    removed unused stuff after major refactor.

--- old/breakup_nee.m
+++
@@ -1,9 +0,0 @@
-
-for y = 1980:2018; tic
-    y
-    i = find(date(1,:)==y);
-    yearData = nee(:,:,i);
-    fname = strcat('./data/nee_year_',num2str(y),'.mat');
-    save(fname,'yearData','-v7.3');
-    toc
-end
