4e0567d7757703391a38239036638b067313fefb
Grey Nearing <grey.nearing@gmail.com>
2018-07-29 19:02:31 -0500

    added Budyko analysis

---
+++ data/in_situ/calculate_allflux_budyko.m
@@ -0,0 +1,79 @@
+%% --- Runtime Environment ------------------------------------------------
+
+clear all; close all; clc;
+restoredefaultpath; 
+addpath(genpath('../../'))
+
+%% --- Load All of the FluxNet and AmeriFlux Data -------------------------
+
+% % load the data
+% load('allflux_Xdata.mat');
+% Xdata(:,1:2,:) = [];
+
+% load the data
+[Xdata,~,~] = load_regression_data('fn',2*365,1);
+
+% dimensions
+[Nt,Nx,Ns] = size(Xdata);
+
+%% --- Calculate Indexes --------------------------------------------------
+
+% init storage
+di = zeros(Ns,1)./0;
+ef = zeros(Ns,1)./0;
+
+% loop through sites
+for s = 1:Ns
+    [di(s),ef(s)] = budyko(Xdata(:,:,s));
+end
+
+%% --- Save Results -------------------------------------------------------
+
+% output directory
+Odir = './extracted/';
+
+% file name
+fname = strcat(Odir,'allflux_Network.mat');
+
+% creat output structure
+Budyko = [ef,di];
+
+% save 
+save(fname,'Budyko');
+
+%% --- Plot Budyko Figure -------------------------------------------------
+
+% Turc-Pike curve
+v = 2;
+Xtp = 0:0.1:10.5;
+Ytp = (1+Xtp.^-v).^(-1/v);
+
+% set up figure
+fig = 1;
+figure(fig); close(fig); figure(fig);
+set(gcf,'color','w');
+set(gcf,'position',[1619 619 1141 486]);
+
+% plot budyko
+h1 = plot([0,1],[0,1],'k-','linewidth',3); hold on
+h2 = plot([1,10],[1,1],'k-','linewidth',3);
+
+% plot turc-pike
+h3 = plot(Xtp,Ytp,'k--','linewidth',3);
+
+% plot site data
+h4 = plot(di,ef,'o','linewidth',3);
+
+% aesthetics
+grid on
+axis([0,5,0,2.2]);
+xlabel('Dryness Index (E_p/P)','fontsize',20);
+ylabel('Evaporative Fraction (E_a/P)','fontsize',20);
+title('Budyko Analysis of Flux Tower Sites','fontsize',22);
+legend([h1,h3,h4],'Budyko','Turc-Pike','Flux Sites');
+set(gca,'fontsize',16);
+
+%% --- END SCRIPT ---------------------------------------------------------
+
+
+
--- tools/budyko.m
+++ tools/budyko.m
@@ -1,34 +1,45 @@
 function [di,ef] = budyko(X)
 
 % grandmas
-cols = 3:9;
+cols = 1:8;
 I = all(~isnan(X(:,cols)'));
 assert(length(I) == size(X,1));
 I = find(I);
+if length(I) < 2*365
+    ef = 0/0;
+    di = 0/0;
+    return
+end
 
 % extract variables
-Pp = X(I,3);            % precipitation                 [mm/d]
-Ta = X(I,4);            % air temperature               [C]
-Pa = X(I,5);            % air pressure                  [kPa]
-Ws = X(I,6);            % wind speed                    [m/s]
-Vp = X(I,7);            % vapor pressure deficit        [hPa]
-Rn = X(I,8);            % net radiation                 [W/m2]
-Qe = X(I,9);            % latent heat                   [W/m2]
+Pp = X(I,1);            % precipitation                 [mm/d]
+Ta = X(I,2);            % air temperature               [C]
+Pa = X(I,3);            % air pressure                  [kPa]
+Ws = X(I,4);            % wind speed                    [m/s]
+Vp = X(I,5);            % vapor pressure deficit        [hPa]
+Rn = X(I,6);            % net radiation                 [W/m2]
+Qe = X(I,7);            % latent heat                   [W/m2]
+Qh = X(I,8);            % sensible heat                 [W/m2]
 
-% slope of saturation vapor pressure curve
-expterm = (17.27*Ta) ./ (Ta+237.3);
-numerat = 4098 * (0.6108 * exp(expterm));
-denomin = (Ta+237.3).^2;
-m  = numerat./denomin;  %                               [~]
+% % slope of saturation vapor pressure curve
+% expterm = (17.27*Ta) ./ (Ta+237.3);
+% numerat = 4098 * (0.6108 * exp(expterm));
+% denomin = (Ta+237.3).^2;
+% m  = numerat./denomin;  %                               [~]
+% 
+% % potential evap - penman
+% Rn = Rn * 0.0864;       % net radiation                 [MJ/m2/d]
+% gv = 2264.705 /1e3;     % latent heat of vaporization   [MJ/kg]
+% g  = 0.0016286*Pa/gv;   % psychormetric constant        [Pa/K]
+% de = Vp/1e1;            % vapor pressure deficit        [kPa]
+% 
+% Ep = m.*Rn + g.*6.43.*(1+0.536.*Ws).*de;
+% Ep = Ep ./ gv.*(m+g);    %                              [mm/d]
 
-% potential evap - penman
-Rn = Rn * 0.0864;       % net radiation                 [MJ/m2/d]
-gv = 2264.705 /1e3;     % latent heat of vaporization   [MJ/kg]
-g  = 0.0016286*Pa/gv;   % psychormetric constant        [Pa/K]
-de = Vp/1e1;            % vapor pressure deficit        [kPa]
-
-Ep = m.*Rn + g.*6.43.*(1+0.536.*Ws).*de;
-Ep = Ep ./ gv.*(m+g);    %                              [mm/d]
+% potential evap from Milly & Dunne (2016)
+Ep = 0.8*(Qe+Qh);
+Ep = Ep * 0.0864;       %                               [MJ/m2/d]
+Ep = Ep / 2.45;         %                               [mm/d]
 
 % actual evap
 Ea = Qe * 0.0864;       %                               [MJ/m2/d]
