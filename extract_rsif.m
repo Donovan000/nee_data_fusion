b01a5fc35f0ac1787ee282dc0278c0993edd2ca6
Donovan000 <donoman15@gmail.com>
2018-07-31 11:17:33 -0400

    Add files via upload

--- extract_rsif.m
+++ extract_rsif.m
@@ -1,170 +1,40 @@
-%% --- Runtime Environment ------------------------------------------------
-
 clear all; close all; clc;
-restoredefaultpath; addpath(genpath(pwd));
-fignum = 0;
+%% ~~~ Load and format RSIF for training at 425 sites ~~~
 
-%% --- Load Data ----------------------------------------------------------
+fprintf('Loading RSIF data...\n'); tic;
+load('./data/RSIF_2007_2016_05N_01L.mat', 'RSIF');
+RSIF = RSIF(:, :, 1:192); % Axe 2015 and 2016
 
-% screen report
-fprintf('Loading data ... '); tic;
+%% ~~~ Create 3D column array for RSIF ~~~
 
-% load fluxnet data
-load('./data/FluxNetDailyData.mat');  % fluxnet daily concatenated matrix
-fluxnetData = allData; clear allData;
+% Initialize  concatenation-compatible matrix
+fprintf('Initializing deposit matrix... \n');
+Nsites = 425;
+Nvars = 1;
+Nsamples = 192;
+matchedRSIF = zeros(Nsamples, Nvars, Nsites);
 
-% load fluxnet site names
-load('./data/FluxNetSiteNames.mat');  
-fluxnetSite = sname; clear sname;
+% Match and pull in data
+fprintf('Populating... \n');
+addpath('./tools');
 
-% dimensions of fluxnet data
-[Nfn,Dfn,Nsites] = size(fluxnetData);
-
-% Load site IDs and coords - removed GH-Ank, IT-BCi, and US-Wkg site info (no dailies)
-siteData = readtable('latlon.csv'); 
-siteLoc = table2cell(siteData);
-siteLoc{1} = 'AR-SLu'; %correction
-
-% Convert siteData to matrices
-siteIDs = cell2mat(siteLoc(:,1));
-lat = cell2mat(siteLoc(:,2));
-lon = cell2mat(siteLoc(:,3));
-
-% Add lat, lon, and RSIF columns to fluxnetData --> allData
-allData = horzcat(fluxnetData(:,1:2,:),nan(Nfn,2,Nsites));  % add lat/lon columns
-allData = horzcat(allData,fluxnetData(:,3:end,:));         
-allData = horzcat(allData,nan(Nfn,1,Nsites));               % add RSIF column
-%allData = horzcat(allData,nan(Nfn,1,Nsites));               % add Noah-MP column
-
-% Input site coordinates in 2 columns for each site
+f = 1;
+row = 1;
 for s = 1:Nsites
-    for d = 1:Nfn
-       allData(d,3,s) = lat(s);
-       allData(d,4,s) = lon(s); 
-    end  
-end
-
-% screen report
-fprintf('. finished; time = %f \n',toc); 
-
-%% --- Geo-Locate RSIF ----------------------------------------------------
-
-% screen report
-fprintf('GEO-Locating RSIF data ... '); tic;
-
-% load RSIF data
-load('./data/RSIF_2007_2016_05N_01L.mat')
-
-% dimensions of RSIF data
-[~,~,Nrsif] = size(RSIF);
-
-% create time indexing vector
-Yrsif = sort(repmat(2007:2016,[1,24]))';
-Drsif = repmat(round(linspace(1,365,24)),[1,10]); Drsif(1) = [];
-
-% creat space indexing vector
-Rlat = (90:-0.5:-90)' - 0.25; Rlat(1) = [];
-Rlon = (-180:0.5:180)' - 0.25; Rlon(1) = [];
-
-% Match latlon to RSIF coords
-for s = 1:Nsites
-
-    % index into RSIF spatial dimensions
-    [dummy,Xdex] = min(abs(Rlon-lon(s))); assert(dummy<=0.25);
-    [dummy,Ydex] = min(abs(Rlat-lat(s))); assert(dummy<=0.25);
-
-    % index into RSIF time dimension
-    for t = 1:Nrsif
-        
-        % index into fluxnet array
-        Iy = find(allData(:,1) == Yrsif(t));
-        Id = find(allData(Iy,2) == Drsif(t));
-        if ~(length(Id) == 1); continue; end
-        
-        % store in fluxnet array
-        allData(Iy(Id),end,s) = RSIF(Ydex,Xdex,t);
-%         assert(~isnan(RSIF(Ydex,Xdex,t)));
-        
+    fprintf('Using data from site %g of %g \n', s, Nsites);
+    [r, c] = halfdeg_site2dex(s);
+    for row = 1:Nsamples
+        matchedRSIF(row, 1, s) = RSIF(r, c, f);
+        f = f + 1;
     end
-        
+    f = 1;
 end
 
-% screen report
-fprintf('. finished; time = %f \n',toc); 
+%% ~~~ Endit ~~~
 
-%% --- Geo-Locate Noah-MP -------------------------------------------------
+fprintf('Saving data as matchedRSIF... \n')
+save('./data/matched_for_training/rsif_matched.mat', 'matchedRSIF');
+fprintf('Finished. That took %g seconds. \n', round(toc, 1));
 
-% screen report
-fprintf('GEO-Locating RSIF data ... '); tic;
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-% % load RSIF data
-% load('./data/conus_nee_noahmp.mat')
-
-% dimensions of RSIF data
-[~,~,Nrsif] = size(RSIF);
-
-% create time indexing vector
-Yrsif = sort(repmat(2007:2016,[1,24]))';
-Drsif = repmat(round(linspace(1,365,24)),[1,10]); Drsif(1) = [];
-
-% creat space indexing vector
-Rlat = (90:-0.5:-90)' - 0.25; Rlat(1) = [];
-Rlon = (-180:0.5:180)' - 0.25; Rlon(1) = [];
-
-% Match latlon to RSIF coords
-for s = 1:Nsites
-
-    % index into RSIF spatial dimensions
-    [dummy,Xdex] = min(abs(Rlon-lon(s))); assert(dummy<=0.25);
-    [dummy,Ydex] = min(abs(Rlat-lat(s))); assert(dummy<=0.25);
-
-    % index into RSIF time dimension
-    for t = 1:Nrsif
-
-        % index into fluxnet array
-        Iy = find(allData(:,1) == Yrsif(t));
-        Id = find(allData(Iy,2) == Drsif(t));
-        if ~(length(Id) == 1); continue; end
-
-        % store in fluxnet array
-        allData(Iy(Id),end,s) = RSIF(Ydex,Xdex,t);
-%         assert(~isnan(RSIF(Ydex,Xdex,t)));
-
-    end
-end
-
-% screen report
-fprintf('. finished; time = %f \n',toc);
-
-%% --- Save Results -------------------------------------------------------
-    
-% screen report
-fprintf('Saving final data matrices ...'); tic
-
-% save concatenated data structures
-save('./data/allDailyData.mat','allData');
-
-% screen report
-fprintf('. finished; time = %f. \n',toc);
-
-%% --- Sanity Check -------------------------------------------------------
-
-% Identify the specific Headers to pull from CSVs
-vnames = [{'YEAR'},{'DOY'},{'LAT'},{'LON'},{'P_ERA'},{'TA_ERA'},{'PA_ERA'},{'SW_IN_ERA'},{'LW_IN_ERA'}...
-    ,{'WS_ERA'},{'LE_F_MDS'},{'H_F_MDS'},{'NEE_CUT_USTAR50'},{'NEE_VUT_USTAR50'},...
-    {'SWC_F_MDS_1'},{'SWC_F_MDS_2'},{'SWC_F_MDS_3'},{'TS_F_MDS_1'},{'TS_F_MDS_2'}...
-    {'TS_F_MDS_3'},{'VPD_ERA'},{'GPP_DT_VUT_USTAR50'},{'GPP_DT_CUT_USTAR50'},{'RSIF'}];
-
-for d = 1:size(allData,2)
-    
-    figure(d); close(d); figure(d)
-    for s = 1:Nsites
-        i = find(~isnan(allData(:,d,s)));
-        a = allData(i,d,s);
-        plot(a); hold on;
-    end
-    title(vnames(d))
-    
-end
-
-%% --- END SCRIPT ---------------------------------------------------------
---
+++ format_nee.m
@@ -0,0 +1,35 @@
+clear all; close all; clc;
+%% ~~~ Compress allflux NEE from daily to 8-day observations ~~~
+
+% Load source data
+fprintf('Loading NEE data...\n'); tic;
+load('./data/in_situ/extracted/allflux_Ydata.mat', 'Ydata');
+
+% Initialize deposit matrix
+Nrows = 384;
+Ncols = 1;
+Nsites = 425;
+matchedNEE = zeros(Nrows, Ncols, Nsites);
+
+fprintf('Populating new matrix matchedNEE...\n');
+f = 1;
+for s = 1:Nsites
+    for r = 1:Nrows
+        if mod(r, 8) == 0
+            matchedNEE(r, :, s) = (sum(Ydata(f:f+7, :, s)))/8;
+            f = f + 7;
+        else
+            matchedNEE(r, :, s) = (sum(Ydata(f:f+6, :, s)))/7;
+            f = f + 6;
+        end
+    end
+    f = 1;
+end
+
+%% ~~~ Endit ~~~
+
+fprintf('Saving...\n');
+save('./data/matched_for_training/nee_matched.mat', 'matchedNEE'); 
+fprintf('Finished. That took %g seconds. \n', round(toc, 1)); 
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
